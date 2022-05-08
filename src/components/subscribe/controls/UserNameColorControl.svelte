7fba910d1f7d2fd45b00f5c394495ed01ee775e2
BruhaBruh <drugsho.jaker@gmail.com>
2022-05-08 14:43:45 +0500

    fix bug with load color in controls

--- src/components/subscribe/controls/UserNameColorControl.svelte
+++ src/components/subscribe/controls/UserNameColorControl.svelte
@@ -20,6 +20,13 @@
     saturation = color[1];
     value = color[2];
   };
+
+  config.subscribe((v) => {
+    const color = chroma.hex(v.usernameColor).hsv();
+    hue = color[0];
+    saturation = color[1];
+    value = color[2];
+  });
 </script>
 
 <TextField title={$LL.subscribeAlerts.controls.usernameColor()} class="mb-4">
